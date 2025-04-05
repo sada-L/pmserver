3f27f55173d207dfc9a39ef8134cfadbd1debe8c
sada-L <g-g2g-g78@mail.ru>
2025-04-05 15:57:36 +0000

    feat: rewrite user controller

--- internal/infrastructure/http/v1/controller/user.go
+++ internal/infrastructure/http/v1/controller/user.go
@@ -1,102 +1,104 @@
 package controller
 
 import (
-	"fmt"
-	"log"
+	"errors"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
 	"github.com/sada-L/pmserver/internal/model"
+	"github.com/sada-L/pmserver/pkg/utils"
 )
 
 type UserController struct {
-	UserService model.UserService
+	us model.UserService
 }
 
-func NewUserController(s model.UserService) *UserController {
-	return &UserController{UserService: s}
+func NewUserController(us model.UserService) *UserController {
+	return &UserController{us: us}
 }
 
-type LoginRequest struct {
-	Email    string `form:"email" binding:"required"`
-	Password string `form:"password" binding:"required"`
+var validate *validator.Validate
+
+func (uc *UserController) LoginUser() http.HandlerFunc {
+	type Input struct {
+		User struct {
+			Email    string `json:"email"`
+			Password string `json:"password"`
+		} `json:"user"`
+	}
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		input := &Input{}
+
+		if err := utils.ReadJSON(r.Body, input); err != nil {
+			utils.ErrorResponse(w, http.StatusBadRequest, err)
+			return
+		}
+
+		user, err := uc.us.Authenticate(r.Context(), input.User.Email, input.User.Password)
+		if err != nil || user == nil {
+			utils.InvalidUserCredentialsError(w)
+			return
+		}
+
+		token, err := utils.GenerateUserToken(user)
+		if err != nil {
+			utils.ServerError(w, err)
+			return
+		}
+
+		user.Token = token
+
+		utils.WriteJSON(w, http.StatusOK, utils.M{"user": user})
+	}
 }
 
-// Login
-//
-//	@Summary		Авторизация.
-//	@Description	Авторизация по логину и паролю.
-//	@Tags			Auth
-//	@Produce		json
-//	@Param			email		formData	string	true	"Email"
-//	@Param			password	formData	string	true	"Password"
-//	@Success		200			{object}	model.User
-//	@Failure		401			{object}	string
-//	@Failure		500			{object}	string
-//	@Router			/login [post]
-func (c *UserController) Login(ctx *gin.Context) {
-	var request LoginRequest
-
-	err := ctx.Bind(&request)
-	if err != nil {
-		log.Print(fmt.Errorf("failed to bind request: %w", err))
-		ctx.JSON(http.StatusBadRequest, gin.H{"Message": err})
-		return
+func (uc *UserController) CreateUser() http.HandlerFunc {
+	type Input struct {
+		User struct {
+			Email    string `json:"email" validate:"required,email"`
+			Username string `json:"username" validate:"required,min=2"`
+			Password string `json:"password" validate:"required,min=8,max=72"`
+		} `json:"user" validate:"required"`
 	}
 
-	user, err := c.UserService.UserByEmail(ctx, request.Email)
-	if err != nil {
-		log.Print(fmt.Errorf("user not found: %w", err))
-		ctx.JSON(http.StatusBadRequest, gin.H{"Message": err})
-		return
+	return func(w http.ResponseWriter, r *http.Request) {
+		input := &Input{}
+
+		if err := utils.ReadJSON(r.Body, input); err != nil {
+			utils.ErrorResponse(w, http.StatusUnprocessableEntity, err)
+		}
+
+		// if err := validate.Struct(input.User); err != nil {
+		// 	utils.ValidationError(w, err)
+		// 	return
+		// }
+
+		user := model.User{
+			Email:    input.User.Email,
+			Username: input.User.Username,
+		}
+
+		user.SetPassword(input.User.Password)
+
+		if err := uc.us.CreateUser(r.Context(), &user); err != nil {
+			switch {
+			case errors.Is(err, model.ErrDuplicateEmail):
+				err = model.ErrorM{"email": {"this email is already in use"}}
+				utils.ErrorResponse(w, http.StatusConflict, err)
+
+			case errors.Is(err, model.ErrDuplicateUsername):
+				err = model.ErrorM{"username": {"this username is already in use"}}
+				utils.ErrorResponse(w, http.StatusConflict, err)
+			default:
+				utils.ServerError(w, err)
+			}
+			return
+		}
+
+		utils.WriteJSON(w, http.StatusCreated, utils.M{"user": user})
 	}
-
-	ctx.JSON(http.StatusOK, user)
-}
-
-type SignUpRequest struct {
-	Name     string `form:"name" binding:"required"`
-	Email    string `form:"email" binding:"required"`
-	Password string `form:"password" binding:"required"`
-}
-
-// Signup
-//
-//	@Summary		Signup.
-//	@Description	Signup
-//	@Tags			Auth
-//	@Produce		json
-//	@Param			name	formData	string	true	"Name"
-//	@Param			email		formData	string	true	"Email"
-//	@Param			password	formData	string	true	"Password"
-//	@Success		200			{object}	model.User
-//	@Failure		401			{object}	string
-//	@Failure		500			{object}	string
-//	@Router			/signup [post]
-func (c *UserController) Signup(ctx *gin.Context) {
-	var request SignUpRequest
-
-	err := ctx.ShouldBind(&request)
-	if err != nil {
-		log.Print(fmt.Errorf("failed to bind request: %w", err))
-		ctx.JSON(http.StatusBadRequest, gin.H{"Message": err})
-		return
-	}
-
-	user := &model.User{
-		Name:     request.Name,
-		Email:    request.Email,
-		Password: request.Password,
-	}
-
-	err = c.UserService.CreateUser(ctx, user)
-	if err != nil {
-		log.Print(fmt.Errorf("server error: %w", err))
-		ctx.JSON(http.StatusInternalServerError, gin.H{"Message": err})
-		return
-	}
-
-	ctx.JSON(http.StatusOK, user)
 }
 
 func (c *UserController) DeleteUser(ctx *gin.Context) {
