ae5587250e324fd0373a914f3232c6a9cadd6820
sada-L <g-g2g-g78@mail.ru>
2025-04-05 15:59:49 +0000

    feat: add fields to user model and change json

--- internal/model/user.go
+++ internal/model/user.go
@@ -2,15 +2,19 @@ package model
 
 import (
 	"context"
+	"fmt"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 type User struct {
-	Id       uint    `json:"id"`
-	Name     string  `json:"name"`
-	Email    string  `json:"email"`
-	Password string  `json:"password"`
-	Cards    []Card  `json:"cards"`
-	Groups   []Group `json:"groups"`
+	Id           uint     `json:"-"`
+	Username     string   `json:"username,omitempty"`
+	Email        string   `json:"email,omitempty"`
+	Token        string   `json:"token,omitempty"`
+	PasswordHash string   `json:"-"`
+	Cards        []*Card  `json:"-"`
+	Groups       []*Group `json:"-"`
 }
 
 type UserRepository interface {
@@ -26,4 +30,21 @@ type UserService interface {
 	UpdateUser(context.Context, *User) error
 	DeleteUser(context.Context, uint) error
 	UserByEmail(context.Context, string) (*User, error)
+}
+
+func (u *User) SetPassword(password string) error {
+	hashBytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return fmt.Errorf("user - SetPassword - GenHash: %w", err)
+	}
+
+	u.PasswordHash = string(hashBytes)
+
+	return nil
+}
+
+func (u *User) VerifyPassword(password string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(u.PasswordHash), []byte(password))
+
+	return err == nil
 }
