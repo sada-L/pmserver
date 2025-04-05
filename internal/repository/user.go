e1b173485480ca31193492b32117cacf038ac46f
sada-L <g-g2g-g78@mail.ru>
2025-04-05 16:01:10 +0000

    feat: change sql query and add error checker

--- internal/repository/user.go
+++ internal/repository/user.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"context"
+	"strings"
 
 	"github.com/sada-L/pmserver/internal/model"
 	"github.com/sada-L/pmserver/pkg/postgres"
@@ -17,12 +18,22 @@ func NewUserRepository(q postgres.Querier) model.UserRepository {
 
 func (ur *userRepository) CreateUser(ctx context.Context, user *model.User) error {
 	query := `
-    INSERT INTO users (name, email, password)
+    INSERT INTO users (username, email, password_hash)
     VALUES ($1,$2,$3) RETURNING id
   `
-
-	args := []interface{}{user.Name, user.Email, user.Password}
-	return ur.q.QueryRowContext(ctx, query, args...).Scan(&user.Id)
+	args := []interface{}{user.Username, user.Email, user.PasswordHash}
+	err := ur.q.QueryRowContext(ctx, query, args...).Scan(&user.Id)
+	if err != nil {
+		switch {
+		case strings.Contains(err.Error(), "users_email_key"):
+			return model.ErrDuplicateEmail
+		case strings.Contains(err.Error(), "users_username_key"):
+			return model.ErrDuplicateUsername
+		default:
+			return err
+		}
+	}
+	return nil
 }
 
 func (r *userRepository) UpdateUser(ctx context.Context, user *model.User) error {
@@ -35,7 +46,7 @@ func (r *userRepository) DeleteUser(ctx context.Context, id uint) error {
 
 func (r *userRepository) UserByEmail(ctx context.Context, email string) (*model.User, error) {
 	userQuery := `
-    SELECT id, name, email, password
+    SELECT id, username, email, password_hash
     FROM users
     WHERE email = $1
   `
@@ -43,8 +54,9 @@ func (r *userRepository) UserByEmail(ctx context.Context, email string) (*model.
 	user := &model.User{}
 	err := r.q.QueryRowContext(ctx, userQuery, email).Scan(
 		&user.Id,
-		&user.Name,
+		&user.Username,
 		&user.Email,
+		&user.PasswordHash,
 	)
 	if err != nil {
 		return nil, err
