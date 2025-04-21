da186ff921c24fad4c33688f6b26d47b429a35ad
sada-L <g-g2g-g78@mail.ru>
2025-04-21 18:48:15 +0300

    style: change query string

--- internal/repository/user.go
+++ internal/repository/user.go
@@ -17,10 +17,8 @@ func NewUserRepository(q postgres.Querier) model.UserRepository {
 }
 
 func (ur *userRepository) CreateUser(ctx context.Context, user *model.User) error {
-	query := `
-    INSERT INTO users (username, email, password_hash)
-    VALUES ($1,$2,$3) RETURNING id
-  `
+	query := `INSERT INTO users (username, email, password_hash) VALUES ($1,$2,$3) RETURNING id `
+
 	args := []interface{}{user.Username, user.Email, user.PasswordHash}
 	err := ur.q.QueryRowContext(ctx, query, args...).Scan(&user.Id)
 	if err != nil {
@@ -45,11 +43,7 @@ func (r *userRepository) DeleteUser(ctx context.Context, id uint) error {
 }
 
 func (r *userRepository) UserByEmail(ctx context.Context, email string) (*model.User, error) {
-	userQuery := `
-    SELECT id, username, email, password_hash
-    FROM users
-    WHERE email = $1
-  `
+	userQuery := ` SELECT id, username, email, password_hash FROM users WHERE email = $1 `
 
 	user := &model.User{}
 	err := r.q.QueryRowContext(ctx, userQuery, email).Scan(
